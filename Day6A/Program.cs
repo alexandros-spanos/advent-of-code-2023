74fa6509b2f2ecaf507bb015f51a31f60598d95f
Alexandros Spanos <ale.spanos@kaizengaming.com>
2023-12-06 13:14:26 +0200

    day 6

---
+++ Day6A/Program.cs
@@ -0,0 +1,26 @@
+﻿var races = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\races-records.txt"));
+var times = ParseLine(races[0]);
+var distances = ParseLine(races[1]);
+var numberOfWays = new List<int>();
+
+for (var index = 0; index < times.Count; index++)
+{
+    numberOfWays.Add(GetNumberOfWays(times[index], distances[index]));
+}
+
+Console.WriteLine(numberOfWays.Aggregate((a, b) => a * b).ToString());
+
+int GetNumberOfWays(int time, int distance)
+{
+    var D = Math.Sqrt(Math.Pow(time, 2) - 4 * distance);
+    var r1 = (int)Math.Ceiling((time + D) / 2);
+    var r2 = (int)Math.Floor((time - D) / 2);
+    return r1 - r2 - 1;
+}
+
+static List<int> ParseLine(string line)
+{
+    return line.Split(':')[1]
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+        .ToList().ConvertAll(int.Parse);
+}
---
+++ Day6B/Program.cs
@@ -0,0 +1,17 @@
+﻿var races = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\races-records.txt"));
+
+Console.WriteLine(GetNumberOfWays(ParseLine(races[0]), ParseLine(races[1])).ToString());
+
+long GetNumberOfWays(long time, long distance)
+{
+    var D = Math.Sqrt(Math.Pow(time, 2) - 4 * distance);
+    var r1 = (long)Math.Ceiling((time + D) / 2);
+    var r2 = (long)Math.Floor((time - D) / 2);
+    return r1 - r2 - 1;
+}
+
+static long ParseLine(string line)
+{
+    return long.Parse(string.Join("", line.Split(':')[1]
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)));
+}
